24fd61a5c47cb666e886f1e03bf018d072a5f098
Matthew Zhou <mattzh1314@gmail.com>
2025-10-22 17:10:29 -0700

    refactor: Flatten package imports for easier pip usage (#89)

--- letta_evals/__init__.py
+++ letta_evals/__init__.py
@@ -3,11 +3,27 @@
 from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as _pkg_version
 
-from letta_evals.graders.rubric import RubricGrader
-from letta_evals.graders.tool import ToolGrader
-from letta_evals.models import GateSpec, RunnerResult, Sample, SuiteSpec
+from letta_evals.decorators import agent_factory, extractor, grader, suite_setup
+from letta_evals.graders import AgentJudgeGrader, Grader, RubricGrader, ToolGrader
+from letta_evals.models import (
+    GateSpec,
+    GradeResult,
+    GraderSpec,
+    MetricAggregate,
+    Metrics,
+    ModelMetrics,
+    RunnerResult,
+    RunStatistics,
+    Sample,
+    SampleResult,
+    SuiteSpec,
+    TargetResult,
+    TargetSpec,
+)
 from letta_evals.runner import Runner, run_suite
-from letta_evals.targets.agent import AgentTarget
+from letta_evals.targets import AgentTarget, Target
+from letta_evals.types import GateMetric, GraderKind, LLMProvider, MetricOp, TargetKind
+from letta_evals.visualization.factory import ProgressStyle, create_progress_callback
 
 try:
     __version__: str = _pkg_version("letta-evals")
@@ -17,11 +33,34 @@ except PackageNotFoundError:
 __all__ = [
     "Sample",
     "SuiteSpec",
-    "RunnerResult",
     "GateSpec",
-    "Runner",
+    "TargetSpec",
+    "GraderSpec",
     "run_suite",
+    "Runner",
+    "Target",
     "AgentTarget",
+    "Grader",
     "ToolGrader",
     "RubricGrader",
+    "AgentJudgeGrader",
+    "RunnerResult",
+    "GradeResult",
+    "SampleResult",
+    "TargetResult",
+    "Metrics",
+    "ModelMetrics",
+    "MetricAggregate",
+    "RunStatistics",
+    "GraderKind",
+    "TargetKind",
+    "MetricOp",
+    "GateMetric",
+    "LLMProvider",
+    "grader",
+    "extractor",
+    "agent_factory",
+    "suite_setup",
+    "ProgressStyle",
+    "create_progress_callback",
 ]
