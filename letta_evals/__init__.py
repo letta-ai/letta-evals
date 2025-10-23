1b6d8ce31274279ab4c0649b04f94572082b4b01
Matt Zhou <mattzh1314@gmail.com>
2025-10-22 17:10:16 -0700

    Remove comments

--- letta_evals/__init__.py
+++ letta_evals/__init__.py
@@ -3,14 +3,8 @@
 from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as _pkg_version
 
-# decorators
 from letta_evals.decorators import agent_factory, extractor, grader, suite_setup
-
-# grader classes
 from letta_evals.graders import AgentJudgeGrader, Grader, RubricGrader, ToolGrader
-
-# core config models
-# result models
 from letta_evals.models import (
     GateSpec,
     GradeResult,
@@ -26,17 +20,9 @@ from letta_evals.models import (
     TargetResult,
     TargetSpec,
 )
-
-# runner
 from letta_evals.runner import Runner, run_suite
-
-# target classes
 from letta_evals.targets import AgentTarget, Target
-
-# types/enums
 from letta_evals.types import GateMetric, GraderKind, LLMProvider, MetricOp, TargetKind
-
-# visualization
 from letta_evals.visualization.factory import ProgressStyle, create_progress_callback
 
 try:
@@ -45,24 +31,19 @@ except PackageNotFoundError:
     __version__ = "0.4.1"
 
 __all__ = [
-    # core config models
     "Sample",
     "SuiteSpec",
     "GateSpec",
     "TargetSpec",
     "GraderSpec",
-    # runner
     "run_suite",
     "Runner",
-    # target classes
     "Target",
     "AgentTarget",
-    # grader classes
     "Grader",
     "ToolGrader",
     "RubricGrader",
     "AgentJudgeGrader",
-    # result models
     "RunnerResult",
     "GradeResult",
     "SampleResult",
@@ -71,18 +52,15 @@ __all__ = [
     "ModelMetrics",
     "MetricAggregate",
     "RunStatistics",
-    # types/enums
     "GraderKind",
     "TargetKind",
     "MetricOp",
     "GateMetric",
     "LLMProvider",
-    # decorators
     "grader",
     "extractor",
     "agent_factory",
     "suite_setup",
-    # visualization
     "ProgressStyle",
     "create_progress_callback",
 ]
